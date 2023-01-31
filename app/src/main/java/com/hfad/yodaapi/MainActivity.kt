8f5a573a2b1860b8e344fea769240035a5069862
johnwaynejr <navuhodonosor11@gmail.com>
2023-01-31 21:59:50 +0300

    Debounce, ProgressBar

--- app/src/main/java/com/hfad/yodaapi/MainActivity.kt
+++ app/src/main/java/com/hfad/yodaapi/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.hfad.yodaapi
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.Placeholder
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -30,16 +40,26 @@ class MainActivity : Activity() {
     private lateinit var queryInput: EditText
     private lateinit var placeholderMessage: TextView
     private lateinit var moviesList: RecyclerView
+    private lateinit var progressBar:ProgressBar
+
+    private val searchRunnable = Runnable { searchRequest() }
 
     private val movies = ArrayList<Movie>()
 
-    private val adapter = MoviesAdapter()
+    private val adapter = MoviesAdapter {
+        if(clickDebounce()) {
+            val intent = Intent(this, PosterActivity::class.java)
+            intent.putExtra("poster", it.image)
+            startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         placeholderMessage = findViewById(R.id.placeholderMessage)
+        progressBar = findViewById(R.id.progressBar)
         searchButton = findViewById(R.id.searchButton)
         queryInput = findViewById(R.id.queryInput)
         moviesList = findViewById(R.id.locations)
@@ -49,34 +69,20 @@ class MainActivity : Activity() {
         moviesList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         moviesList.adapter = adapter
 
-        searchButton.setOnClickListener {
-            if (queryInput.text.isNotEmpty()) {
-                imdbService.findMovie(queryInput.text.toString()).enqueue(object : Callback<MoviesResponse> {
-                    override fun onResponse(call: Call<MoviesResponse>,
-                                            response: Response<MoviesResponse>) {
-                        if (response.code() == 200) {
-                            movies.clear()
-                            if (response.body()?.results?.isNotEmpty() == true) {
-                                movies.addAll(response.body()?.results!!)
-                                adapter.notifyDataSetChanged()
-                            }
-                            if (movies.isEmpty()) {
-                                showMessage(getString(R.string.nothing_found), "")
-                            } else {
-                                showMessage("", "")
-                            }
-                        } else {
-                            showMessage(getString(R.string.something_went_wrong), response.code().toString())
-                        }
-                    }
+        searchButton.setOnClickListener {searchRequest()}
 
-                    override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
-                        showMessage(getString(R.string.something_went_wrong), t.message.toString())
-                    }
-
-                })
+        queryInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
-        }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                searchDebounce()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
     }
 
     private fun showMessage(text: String, additionalMessage: String) {
@@ -93,5 +99,72 @@ class MainActivity : Activity() {
             placeholderMessage.visibility = View.GONE
         }
     }
+
+    private fun hideKeyboard() {
+        this.currentFocus?.let { view ->
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun searchRequest(){
+        if (queryInput.text.isNotEmpty()) {
+            hideKeyboard()
+            placeholderMessage.visibility = View.GONE
+            moviesList.visibility = View.GONE
+            progressBar.visibility = View.VISIBLE
+            imdbService.findMovie(queryInput.text.toString()).enqueue(object : Callback<MoviesResponse> {
+                override fun onResponse(call: Call<MoviesResponse>,
+                                        response: Response<MoviesResponse>) {
+                    progressBar.visibility = View.GONE // Прячем ProgressBar после успешного выполнения запроса
+                    if (response.code() == 200) {
+                        movies.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            moviesList.visibility = View.VISIBLE
+                            movies.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                        }
+                        if (movies.isEmpty()) {
+                            showMessage(getString(R.string.nothing_found), "")
+                        } else {
+                            showMessage("", "")
+                        }
+                    } else {
+                        showMessage(getString(R.string.something_went_wrong), response.code().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
+                    showMessage(getString(R.string.something_went_wrong), t.message.toString())
+                }
+
+            })
+        }
+    }
+
+    companion object {
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
+        private const val SEARCH_DEBOUNCE_DELAY = 2000L
+    }
+
+    private var isClickAllowed = true
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    private fun clickDebounce() : Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
+    }
+
+    private fun searchDebounce() {
+        handler.removeCallbacks(searchRunnable)
+        handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
+    }
+
 }
 
--- app/src/main/java/com/hfad/yodaapi/MoviesAdapter.kt
+++ app/src/main/java/com/hfad/yodaapi/MoviesAdapter.kt
@@ -1,9 +1,12 @@
 package com.hfad.yodaapi
 
+import android.content.Intent
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 
-class MoviesAdapter() : RecyclerView.Adapter<MovieViewHolder>() {
+class MoviesAdapter(val clickListener: MovieClickListener) : RecyclerView.Adapter<MovieViewHolder>() {
 
     var movies = ArrayList<Movie>()
 
@@ -11,7 +14,12 @@ class MoviesAdapter() : RecyclerView.Adapter<MovieViewHolder>() {
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         holder.bind(movies.get(position))
+        holder.itemView.setOnClickListener { clickListener.onMovieClick(movies.get(position)) }
     }
 
     override fun getItemCount(): Int = movies.size
+
+    fun interface MovieClickListener {
+        fun onMovieClick(movie: Movie)
+    }
 }
